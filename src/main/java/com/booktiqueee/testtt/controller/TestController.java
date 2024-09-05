86c798bc09e39e42c249d43f1b42bfab41ee99bc
misukleee <altnrdl625@naver.com>
2024-09-05 20:58:27 +0900

    로그 확인

--- src/main/java/com/booktiqueee/testtt/controller/TestController.java
+++ src/main/java/com/booktiqueee/testtt/controller/TestController.java
@@ -1,5 +1,19 @@
 package com.booktiqueee.testtt.controller;
 
-public class TestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("testtt")
+public class TestController {
+	
+	@GetMapping("name")
+	public String name() {
+		log.debug("로그로그");
+		return "dd";
+	}
 }
